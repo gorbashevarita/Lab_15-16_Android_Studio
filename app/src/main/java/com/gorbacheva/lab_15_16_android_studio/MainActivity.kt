512ceccc63fe6948aeb36ec588e61a991e33728a
Margarita <gorbashevarira@gmail.com>
2026-03-25 16:06:03 +0300

    Initial commit

---
+++ app/src/main/java/com/gorbacheva/lab_15_16_android_studio/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.gorbacheva.lab_15_16_android_studio
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.gorbacheva.lab_15_16_android_studio.ui.theme.Lab_1516_Android_StudioTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Lab_1516_Android_StudioTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Lab_1516_Android_StudioTheme {
+        Greeting("Android")
+    }
+}
